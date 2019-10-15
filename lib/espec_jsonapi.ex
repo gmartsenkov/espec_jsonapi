69bcf8fe59a11625709288f599dc5fe0fcea9ab2
Georgi Martsenkov <georgi.martsenkov@vodafone.com>
2019-10-15 22:47:39 +0100

    adds have_attribute assertion

--- lib/espec_jsonapi.ex
+++ lib/espec_jsonapi.ex
@@ -1,7 +1,9 @@
 defmodule EspecJsonapi do
   alias EspecJsonapi.Assertions.HaveId
   alias EspecJsonapi.Assertions.HaveType
+  alias EspecJsonapi.Assertions.HaveAttribute
 
   def have_id(id), do: {HaveId, id}
   def have_type(id), do: {HaveType, id}
+  def have_attribute(id), do: {HaveAttribute, id}
 end
---
+++ test/assertions/have_attribute_test.exs
@@ -0,0 +1,41 @@
+defmodule EspecJsonapi.Assertions.HaveAttributeTest do
+  use ExUnit.Case
+  alias EspecJsonapi.Assertions.HaveAttribute
+
+  test "match when subject is an empty map" do
+    assert HaveAttribute.match(%{}, "user") == {false, %{}}
+  end
+
+  test "match when subject is another type" do
+    assert HaveAttribute.match("invalid", "user") == {false, "invalid"}
+  end
+
+  test "match when the subject id matches" do
+    subject = %{ "attributes" => %{ "name" => "John", "age" => 17 } }
+    assert HaveAttribute.match(subject, "name") == {true, subject}
+  end
+
+  test "success_message when positive is true" do
+    subject = %{ "attributes" => %{ "name" => "John" } }
+    expected = "`%{\"attributes\" => %{\"name\" => \"John\"}}` has attribute name."
+    assert HaveAttribute.success_message(subject, "name", nil, true) == expected
+  end
+
+  test "success_message when positive is false" do
+    subject = %{ "attributes" => %{ "name" => "John" } }
+    expected = "`%{\"attributes\" => %{\"name\" => \"John\"}}` does not have attribute age."
+    assert HaveAttribute.success_message(subject, "age", nil, false) == expected
+  end
+
+  test "error_message when positive is true" do
+    subject = %{ "attributes" => %{ "name" => "John" } }
+    expected = "Expected `%{\"attributes\" => %{\"name\" => \"John\"}}` to have attribute age."
+    assert HaveAttribute.error_message(subject, "age", nil, true) == expected
+  end
+
+  test "error_message when positive is false" do
+    subject = %{ "attributes" => %{ "name" => "John" } }
+    expected = "Expected `%{\"attributes\" => %{\"name\" => \"John\"}}` not to have attribute name."
+    assert HaveAttribute.error_message(subject, "name", nil, false) == expected
+  end
+end
