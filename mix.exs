02a4499786ac6afa21978e6870e567dc22817bad
Georgi Martsenkov <georgi.martsenkov@vodafone.com>
2019-10-18 10:10:01 +0100

    add hex related info

--- mix.exs
+++ mix.exs
@@ -4,6 +4,8 @@ defmodule EspecJsonapi.MixProject do
   def project do
     [
       app: :espec_jsonapi,
+      description: "JSONAPI matchers for ESpec",
+      package: package(),
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
@@ -24,6 +26,13 @@ defmodule EspecJsonapi.MixProject do
       {:espec, ">= 0.0.0" }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/gmartsenkov/espec_jsonapi"},
     ]
   end
 end
